f6fd7075c2d9dd5c51a5b8d9ae6bbf58d2209ccb
Dobriy4488 <paninevgeny@yahoo.com>
2025-03-16 19:03:54 +0300

    Add files via upload

---
+++ func-brainfuck.csproj (1)/BrainfuckLoopCommands.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace func.brainfuck
+{
+    public class BrainfuckLoopCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            Dictionary<int, int> loopsByStart = new Dictionary<int, int>();
+            Stack<int> currentLoopsByStart = new Stack<int>();
+            vm.RegisterCommand('[', b =>
+            {
+                if (loopsByStart.Count == 0)
+                {
+                    Stack<int> startOfLoops = new Stack<int>();
+                    startOfLoops.Push(b.InstructionPointer);
+                    for (int tempIndex = b.InstructionPointer + 1; tempIndex != b.Instructions.Length; tempIndex++)
+                    {
+                        if (b.Instructions[tempIndex] == '[')
+                            startOfLoops.Push(tempIndex);
+                        else if (b.Instructions[tempIndex] == ']')
+                            loopsByStart[startOfLoops.Pop()] = tempIndex;
+                    }
+                }
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.InstructionPointer = loopsByStart[b.InstructionPointer];
+                else
+                    currentLoopsByStart.Push(b.InstructionPointer);
+            });
+            vm.RegisterCommand(']', b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                    b.InstructionPointer = currentLoopsByStart.Peek();
+                else
+                    currentLoopsByStart.Pop();
+            });
+        }
+    }
+}
---
+++ func-brainfuck.csproj (1)/VirtualMachine.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace func.brainfuck
+{
+    public class VirtualMachine : IVirtualMachine
+    {
+        public VirtualMachine(string program, int memorySize)
+        {
+            Instructions = program;
+            Memory = new byte[memorySize];
+            InstructionPointer = 0;
+            MemoryPointer = 0;
+            InstructionSet = new Dictionary<char, Action<IVirtualMachine>>();
+        }
+
+        public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
+        {
+            InstructionSet.Add(symbol, execute);
+        }
+
+        protected Dictionary<char, Action<IVirtualMachine>> InstructionSet { get; set; }
+        public string Instructions { get; }
+        public int InstructionPointer { get; set; }
+        public byte[] Memory { get; }
+        public int MemoryPointer { get; set; }
+
+        public void Run()
+        {
+            while (InstructionPointer < Instructions.Length)
+            {
+                if (InstructionSet.TryGetValue(Instructions[InstructionPointer], out var;
+                    action(this);
+                InstructionPointer++;
+            }
+        }
+    }
+}
